5cd4662d68b3d05d13befb0138bad174e1386991
canciller <emilio.lopez.gabriel.emilio@gmail.com>
2018-12-17 20:21:20 -0600

    Created script to control pulseaudio default sink

---
+++ i3/scripts/pulseaudio-control.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function get_default_sink {
+	pactl list sinks short | grep RUNNING | awk '{print $1;}'
+}
+
+sink="$(get_default_sink)"
+[ -z "$sink" ] && exit 1
+
+case "$1" in
+	--volume-up) pactl set-sink-volume "$sink" +5%;;
+	--volume-down) pactl set-sink-volume "$sink" -5%;;
+	--volume-toggle) pactl set-sink-mute "$sink" toggle;;
+	*) exit 1;;
+esac
+
+exit 0
