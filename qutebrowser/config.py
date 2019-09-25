865d7af23748a9d6373064f8acf531fe9103a1fa
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2019-09-25 18:39:37 -0500

    Created set_default_searchengine function

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -10,3 +10,22 @@ themes.dracula.draw.blood(c, {
         'size': 10
     }
 })
+
+searchengines = {
+        'google': 'https://www.google.com/search?hl=en&q={}',
+        'duckduckgo': 'https://duckduckgo.com/?q={}',
+        'archwiki': 'https://wiki.archlinux.org/?search={}'
+        }
+
+
+def set_default_searchengine(key):
+    if key in searchengines:
+        searchengines['DEFAULT'] = searchengines[key]
+        del searchengines[key]
+    c.url.searchengines = searchengines
+
+set_default_searchengine('google')
+
+c.url.start_pages = [
+        'https://www.google.com'
+        ]
