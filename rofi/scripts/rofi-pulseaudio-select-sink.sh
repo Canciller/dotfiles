6e056ac0e36a09ee5e40ac8d16cb26eacb4022c3
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2019-09-26 17:54:10 -0500

    Added function to move sink inputs

--- rofi/scripts/rofi-pulseaudio-select-sink.sh
+++ rofi/scripts/rofi-pulseaudio-select-sink.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+move_sink_inputs() {
+    sink="$1"
+    [ -n "$sink" ] || return 1
+
+    sink_inputs=$(pactl list sink-inputs) || return 1
+
+    while read -r sink_input; do
+        index=$(echo "$sink_input" | grep -oP "\d+$")
+        pactl move-sink-input "$index" "$sink" || return 1
+    done < <(echo "$sink_inputs" | grep "Sink Input")
+}
+
 list_sinks() {
     sinks=$(pactl list sinks short) || return 1
     echo "$sinks" | sed -e "s/\t/\ /g"
@@ -10,7 +22,8 @@ select_sink() {
     sink="$(echo "$sink" | cut -f 1 -d " ")"
     [ -n "$sink" ] || return 1
 
-    pactl set-default-sink $sink
+    pactl set-default-sink $sink || return 1
+    move_sink_inputs $sink || return 1
 }
 
 case "$1" in
