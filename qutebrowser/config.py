25da666756947549c7b1c0899909de703e375d10
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2020-04-23 19:47:16 -0500

    Fixed font error

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,16 +1,3 @@
-import themes.dracula.draw
-
-themes.dracula.draw.blood(c, {
-    'spacing': {
-        'vertical': 6,
-        'horizontal': 8
-    },
-    'font': {
-        'family': 'SauceCodePro Nerd Font, Monospace',
-        'size': 10
-    }
-})
-
 searchengines = {
         'google': 'https://www.google.com/search?hl=en&q={}',
         'duckduckgo': 'https://duckduckgo.com/?q={}',
