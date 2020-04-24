77950c2b034397db99ef2787edf933d646657d12
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2020-04-23 19:24:27 -0500

    Created new theme for rofi-session.sh

--- rofi/scripts/rofi-session.sh
+++ rofi/scripts/rofi-session.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-OPTION=$(echo -e \
-"1. Poweroff
-2. Reboot 
-3. Suspend
-4. Hibernate
-5. Logout" | rofi-dmenu | cut -c1)
+rofi_command="rofi -theme ~/.config/rofi/themes/session.rasi"
 
-case $OPTION in
-1) systemctl poweroff;;
-2) systemctl reboot;;
-3) systemctl suspend;;
-4) systemctl hibernate;;
-5) i3lock;;
+power_off=""
+reboot=""
+lock=""
+suspend="鈴"
+log_out=""
+
+options="$power_off\n$reboot\n$lock\n$suspend\n$log_out"
+
+chosen="$(echo -e "$options" | $rofi_command -dmenu -selected-row 2)"
+case $chosen in
+    $power_off)
+        promptmenu --yes-command "systemctl poweroff" --query "Shutdown?"
+        ;;
+    $reboot)
+        promptmenu --yes-command "systemctl reboot" --query "Reboot?"
+        ;;
+    $lock)
+        light-locker-command -l
+        ;;
+    $suspend)
+        mpc -q pause
+        amixer set Master mute
+        systemctl suspend
+        ;;
+    $log_out)
+        i3-msg exit
+        ;;
 esac
