0ee1111d26921372179c1288e34b1fe7406220eb
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2023-03-08 21:39:28 -0600

    feat: create utils/map

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,12 +1,6 @@
 local cmd = vim.cmd
 
-local function map(mode, lhs, rhs, opts)
-  local options = { noremap=true, silent=true }
-  if opts then
-    options = vim.tbl_extend('force', options, opts)
-  end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
+map = require('utils/map')
 
 vim.g.mapleader = ','
 
